96bd8a10d8811536822922f4f91331c2358431c8
Gabriel Schultz <gabikremer2006@gmail.com>
2024-11-26 23:50:31 -0300

    ssfsdf

--- Tetre.cpp
+++ Tetre.cpp
@@ -7,15 +7,12 @@ int addCont(int x, int y){
         return xy*0.5;
     return xy*0.8;
 }
-int addVont(int x, int y){
-    int xy = x*y;
-    if(xy>1000)
-        return xy*0.5;
-    return xy*0.8;
-}
 
 int main(){
     int x, y;
-    cin >> x >> y;
+    for(int i = 0; i<10; i++)
+        cin >> x >> y;
+    for(int i = 0; i<10; i++)
+        cin >> x >> y;
     cout << addCont(x, y) << endl;
 }
